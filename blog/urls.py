011bba8c93ad21dcbd78a87449163cef4c6ea1da
ence1ad <romanrusl@mail.ru>
2023-07-12 16:48:18 +0300

    Add class PostsByCategory to vievs, for get post by category

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import get_category, Home, get_post
+from .views import PostByCategory, Home, get_post
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('category/<str:slug>/', get_category, name='category'),
+    path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
     path('post/<str:slug>/', get_post, name='post'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -7,19 +7,32 @@ class Home(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 4
 
-    def get_context_data(self, object_list=None, **kwargs):
+    def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Classic Blog Design'
         return context
 
-# def index(request):
-#     return render(request, template_name='blog/index.html')
+
+class PostByCategory(ListView):
+    template_name = 'blog/posts_by_cat.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
 
 
 def get_category(request, slug):
     return render(request, template_name='blog/category.html')
+
 
 def get_post(request, slug):
     return render(request, template_name='blog/category.html')
