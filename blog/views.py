002c3c91918a7ce45176c3317c874e46bb0da1ed
ence1ad <romanrusl@mail.ru>
2023-07-12 13:16:19 +0300

    Create views with class Home for index page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Category, Post, Tag
 
 
-# Create your views here.
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
 
-def index(request):
-    return render(request, template_name='blog/index.html')
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic Blog Design'
+        return context
+
+# def index(request):
+#     return render(request, template_name='blog/index.html')
+
 
 def get_category(request, slug):
+    return render(request, template_name='blog/category.html')
+
+def get_post(request, slug):
     return render(request, template_name='blog/category.html')
